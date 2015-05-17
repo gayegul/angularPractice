0bc449971f419a3151b74c34140a0eca039b076e
Gaye Bulut <gayegul@gmail.com>
2015-05-16 22:43:31 -0700

    delete extras

--- js/app.js
+++
@@ -1,1 +0,0 @@
-angular.module('myApp', []).controller('MyController', function($scope, $timeout) {  $scope.clock = {};  var updateClock = function() {    $scope.clock.now = new Date();    $timeout(function() {      updateClock();    }, 1000);  };  updateClock();});
