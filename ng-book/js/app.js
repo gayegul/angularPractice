3a1c953e36ae3f2858261bd14c0449c683f145dd
Gaye Bulut <gayegul@gmail.com>
2015-05-16 22:42:09 -0700

    arrange files

---
+++ ng-book/js/app.js
@@ -0,0 +1,1 @@
+angular.module('myApp', []).controller('MyController', function($scope, $timeout) {  $scope.clock = {};  var updateClock = function() {    $scope.clock.now = new Date();    $timeout(function() {      updateClock();    }, 1000);  };  updateClock();});
